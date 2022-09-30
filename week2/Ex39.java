809cd8ddee6fd12d7f0bebf6b8f972a713362a6b
Maarikal <maarika.lukk@khk.ee>
2022-09-30 10:42:57 +0300

    Week 2 - Exercise 39: Printing stars, square, rectangle, left-aligned triangle

--- week2/Ex39.java
+++ week2/Ex39.java
@@ -6,20 +6,66 @@ public class Ex39 {
         // System.out.print("*");
         // call this command amount times
         int times = 1;
-        while(true){
+        while(times <= amount){
             System.out.print("*");
-            if(times ==amount) {
-                break;
-            }
             times++;
         }
         System.out.println();
     }
 
+    private static void printSquare (int sideSize) {
+        int row = 1;
+        while (row <= sideSize) {
+            int field = 1;
+            while(field <= sideSize) {
+                System.out.print("* ");
+                field++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+
+    private static void printTriangle(int size) {
+        int row = 1;
+        while (row <= size) {
+            int field = 1;
+            while (field <= row) {
+                System.out.print("*");
+                field++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+
+
+    private static void printRectangle(int width, int height){
+        int row = 1;
+        while (row <= height) {
+            int field = 1;
+            while (field <= width) {
+                System.out.print("*");
+                field++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println("----Exercise 39.1-----");
         printStars(5);
         printStars(3);
         printStars(9);
-    }
 
+        System.out.println("-----Exercise 39.2-----");
+        printSquare(4);
+
+        System.out.println("-----Exercise 39.3------");
+        printRectangle(17,3);
+
+        System.out.println("-----Exercise 39.4----");
+        printTriangle(4);
+    }
 }
