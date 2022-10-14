62bd0a160ccdc03d10d8494aeaf59e3cec539704
Maarikal <maarika.lukk@khk.ee>
2022-10-14 09:05:46 +0300

    Week 4 - Exercise 73: Product

--- week4/Product.java
+++ week4/Product.java
@@ -5,6 +5,11 @@ public class Product {
     private double priceAtStart;
     private int amountAtStart;
 
+    // loodud Generate - Getter, lisasime juurde this., sest nii pidi ilusam olema
+    public double getPriceAtStart() {
+        return this.priceAtStart;
+    }
+
     public Product(String nameAtStart, double priceAtStart, int amountAtStart){
         this.priceAtStart = priceAtStart;
         this.nameAtStart = nameAtStart;
@@ -14,4 +19,5 @@ public class Product {
     public void printProduct() {
         System.out.println(this.nameAtStart + ", price " + this.priceAtStart + ", amount " + this.amountAtStart);
     }
+
 }
