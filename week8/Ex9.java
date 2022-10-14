4f33479a82248b4c011f96eb3aa5b2e979c149cc
Maarikal <maarika.lukk@khk.ee>
2022-10-14 11:06:44 +0300

    Week 8 - Exercise 9.2: The Owner, Based of the Registration Plate and Exercise 9.3: More for the Vehicle Register

--- week8/Ex9.java
+++ week8/Ex9.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 
 public class Ex9 {
     public static void main(String[] args) {
+
+        System.out.println("----Exercise 9.1-----");
         RegistrationPlate reg1 = new RegistrationPlate("FI", "ABC-123");
         RegistrationPlate reg2 = new RegistrationPlate("FI", "UXE-465");
         RegistrationPlate reg3 = new RegistrationPlate("D", "B WQ-431");
@@ -28,5 +30,29 @@ public class Ex9 {
         System.out.println(owners.get(new RegistrationPlate("FI", "ABC-123")));
         System.out.println(owners.get(new RegistrationPlate("D", "B WQ-431")));
         // if the hashCode hasn't been overwritten, the owners are not found
+
+        System.out.println();
+        System.out.println("-----Exercise 9.2-----");
+        RegistrationPlate reg4 = new RegistrationPlate("EST", "ABC124");
+        RegistrationPlate reg5 = new RegistrationPlate("EST", "OOO555");
+
+        VehicleRegister register = new VehicleRegister();
+        register.add(reg4, "Mari");
+        register.add(reg5, "Meelis");
+
+        System.out.println(register.get(reg1));
+        System.out.println(register.get(reg2));
+        System.out.println(register.get(reg3));
+        System.out.println(register.get(reg4));
+        System.out.println(register.get(reg5));
+
+        //  register.delete(reg2);
+        System.out.println(register.getOwners());
+
+        System.out.println();
+        System.out.println("---Exercise 9.3----");
+        register.add(reg2, "Tom");
+        register.printRegistrationPlates();
+        register.printOwners();
     }
 }
